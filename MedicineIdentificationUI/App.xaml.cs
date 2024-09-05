f5f875ba7c5f293435d87ec0c2811f35abb32d57
Nishan Shrestha <NishanShrestha9909@gmail.com>
2024-09-05 13:31:40 +0545

    Initial commit for Medicine Identification System

---
+++ MedicineIdentificationUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicineIdentificationUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MedicineIdentificationUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicineIdentificationUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
