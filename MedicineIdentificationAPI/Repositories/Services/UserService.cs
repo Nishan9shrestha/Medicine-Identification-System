71a7b165c3801fef3b76b815047aa86d9ce33674
Nishan Shrestha <NishanShrestha9909@gmail.com>
2024-09-13 12:41:04 +0545

    Repositories along with required services and interfaces are added

---
+++ MedicineIdentificationAPI/Repositories/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using MedicineIdentificationAPI.Models;
+using MedicineIdentificationAPI.Repositories.Interfaces;
+
+namespace MedicineIdentificationAPI.Repositories.Services
+{
+    public class UserService : IUserRepository
+    {
+        public Task AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
