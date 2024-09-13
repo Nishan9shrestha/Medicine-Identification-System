26c683ce1e8d02769702bbaa93f5b08be23113b1
Nishan Shrestha <NishanShrestha9909@gmail.com>
2024-09-13 12:37:55 +0545

    Repositories along with required services and interfaces are added

---
+++ MedicineIdentificationAPI/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using MedicineIdentificationAPI.Models;
+
+namespace MedicineIdentificationAPI.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(Guid userId); // Read a user by ID
+        Task<IEnumerable<User>> GetAllUsersAsync(); // Get all users
+        Task AddUserAsync(User user); // Create a new user
+        Task UpdateUserAsync(User user); // Update user info
+        Task DeleteUserAsync(Guid userId); // Delete a user
+        Task<User> GetUserByEmailAsync(string email); // Retrieve user by email (for authentication)
+
+    }
+}
