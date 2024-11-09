5d4b3d1bd9ef132685f27dbdfede07a0a0d81876
Nishan Shrestha <NishanShrestha9909@gmail.com>
2024-11-09 16:04:37 +0545

    ML  model is added in the project

---
+++ MedicineIdentificationAPI/MlModels/Classes/MedicineDetails.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+       
+
+namespace Model_Train.Classes
+    {
+        public class MedicineDetails
+        {
+            [Name("Medicine Name")]
+            public string MedicineName { get; set; }
+
+            [Name("Composition")]
+            public string Composition { get; set; }
+
+            [Name("Uses")]
+            public string Uses { get; set; }
+
+            [Name("Side_effects")]
+            public string SideEffects { get; set; }
+
+            [Name("Image URL")]
+            public string ImageUrl { get; set; }
+
+            [Name("Manufacturer")]
+            public string Manufacturer { get; set; }
+
+            [Name("Excellent Review %")]
+            public int ExcellentReview { get; set; }
+
+            [Name("Average Review %")]
+            public int AverageReview { get; set; }
+
+            [Name("Poor Review %")]
+            public int PoorReview { get; set; }
+        }
+    }
+
+
---
+++ MedicineIdentificationAPI/MlModels/Methods/LoadMedicineDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using Model_Train.Classes;
+
+namespace Model_Train.Methods
+{
+    public class MedicineDetailsLoader  // Renamed the class to avoid conflict
+    {
+        public List<MedicineDetails> LoadDetails(string csvFilePath)  // Renamed the method
+        {
+            using var reader = new StreamReader(csvFilePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csv.GetRecords<MedicineDetails>().ToList();
+            return records;
+        }
+    }
+}
